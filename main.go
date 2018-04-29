bea37463f29e4eaeb69d8129260899fcb5038521
mehrati <geekoffmail@gmail.com>
2018-04-29 19:42:30 +0430

    add gobuild

--- main.go
+++ main.go
@@ -1,81 +1,49 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"go/build"
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
-	"sort"
-	"strings"
-
 	"gopkg.in/fatih/set.v0"
 )
 
 func main() {
+	s := set.New()
 	dirs := GetAllDirs()
-	files := GetAllFiles(dirs)
-	err := ExecGoImports(files)
-	if err != nil {
-		fmt.Println(err)
+	for _, d := range dirs {
+		imports, _ := GetImport(d)
+		for _,i := range imports {
+			s.Add(i)
+		}
 	}
-	deps := GetDeps(files)
-	ShowDeps(deps)
+	list := s.List()
+	ShowImports(list)
+
 }
+
+func GetImport(path string) ([]string, error) {
+	pkg, err := build.ImportDir(path, 0)
+	if err != nil {
+		return nil, err
+	}
+	return pkg.Imports, nil
+}
+
 // TODO
-func GetNonStdDeps(deps []string) []string{
+func GetNonStdDeps(deps []string) []string {
 
 	return nil
-}
-
-func GetDeps(files []string) []string {
-	RES := set.New()
-	for _, file := range files {
-		fo, err := os.Open(file)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer fo.Close()
-		scanner := bufio.NewScanner(fo)
-	br:
-		for scanner.Scan() {
-			t := scanner.Text()
-			if strings.HasPrefix(t, "import(") || strings.HasPrefix(t, "import (") {
-				RES.Add(TrimImport(t))
-				for scanner.Scan() {
-					t = scanner.Text()
-					if strings.Contains(t, ")") {
-						break br
-					}
-					RES.Add(TrimImport(t))
-				}
-			} else if strings.HasPrefix(t, "import") {
-				RES.Add(TrimImport(t))
-				break br
-			}
-		}
-		if err := scanner.Err(); err != nil {
-			log.Fatal(err)
-		}
-	}
-	listi := RES.List()
-	list := []string{}
-	for _, l := range listi {
-		s := l.(string)
-		if s != "" && s != "import (" && s != "import(" {
-			list = append(list, TrimImport(s))
-		}
-	}
-	RES.Clear()
-	sort.Strings(list)
-	return list
 }
 
 func GetAllDirs() []string {
 	wd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
+	}
+	if len(os.Args) > 1 {
+		wd = os.Args[1]
 	}
 	listdirs := []string{wd}
 	i := 0
@@ -109,61 +77,8 @@ func GetListDir(dir string) ([]string, error) {
 	return listdir, nil
 }
 
-func GetListFile(dir string) ([]string, error) {
-	listfile := []string{}
-	list, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return nil, err
-	}
-	for _, l := range list {
-		if !l.IsDir() && strings.HasSuffix(l.Name(), ".go") {
-			listfile = append(listfile, dir+"/"+l.Name())
-		}
-	}
-	return listfile, nil
-}
-
-func GetAllFiles(dirs []string) []string {
-	files := []string{}
-	for _, d := range dirs {
-		file, err := GetListFile(d)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, f := range file {
-			files = append(files, f)
-		}
-	}
-	return files
-}
-
-func ShowDeps(deps []string) {
-	for _, d := range deps {
+func ShowImports(imp []interface{}) {
+	for _, d := range imp {
 		fmt.Println(d)
-	}
-}
-
-func ExecGoImports(files []string) error {
-	path, err := exec.LookPath("goimports")
-	if err != nil {
-		return err
-	}
-	for _, f := range files {
-		err = exec.Command(path, "-w", f).Run()
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func TrimImport(str string) string {
-	if strings.Contains(str, "\"") {
-		s := ""
-		s = strings.TrimLeft(str, str[:strings.Index(str, "\"")+1])
-		s = strings.TrimRight(s, str[strings.LastIndex(str, "\""):])
-		return s
-	} else {
-		return str
 	}
 }
